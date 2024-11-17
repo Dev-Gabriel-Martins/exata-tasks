cd99252347284954442032822e876b35d38f4b44
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-11-17 10:45:42 -0300

    Add Middleware RoleTasksCheck

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\TaskController;
+use App\Http\Middleware\RoleTasksCheck;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Auth/Login');
 });
 
 Route::get('/dashboard', function () {
@@ -19,9 +15,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+     //Rotas Profile
+     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+     
+     //Rotas tasks protegia por Miidleware
+     Route::resource('tasks', TaskController::class)->middleware(RoleTasksCheck::class);
 });
+
+
+
 
 require __DIR__.'/auth.php';
