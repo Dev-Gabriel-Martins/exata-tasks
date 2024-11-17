44e4319c5c3bbc1551d2d87936406f8b65ee27ef
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-11-17 10:52:00 -0300

    feat: add Gate 'role-tasks-check' and 'admin-check'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +25,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        Gate::define('role-tasks-check', function(User $user, Task $task){
+            return $user->role === 'admin' || $user->id === $task->user_id
+            ? Response::allow()
+            : Response::deny('Você não tem permissão de acesso');
+        });
+
+        Gate::define('admin-check', function(User $user){
+            return $user->role === 'admin'
+            ? Response::allow()
+            : Response::deny('Você não tem permissão de acesso');
+        });
     }
 }
