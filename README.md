e4efbf2d18ae35db02fe6d473b3b6d59e89fef21
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-11-20 03:26:39 -0300

    feat: create setup script start.bat

--- README.md
+++ README.md
@@ -1,16 +1,51 @@
-# Bem vindo ao Exata-Tasks
 
-## Requisitos:
-#### | PHP >= 8.2.0 | Composer >= 2.7.6 | Laravel >= 11.0 | MySQL = 8.0.37 |
+
+# Bem vindo ao Exata-Tasks
+Fico muito feliz que vc estejá aqui, fiz o Exata-Tasks com muito carrinho e dei o meu melhor, espero que goste!
+
+#### Usuario gerados pelo Seeder:
+```cmd
+Role admin:
+usuario: admin@admin.com            
+senha: admin123
+
+Role user:
+usuario: teste@teste.com          
+senha: teste123
+```
 
 ## Instalação
+### Crie um pequeno script para ajudar na instação, ele sobe os containers, roda as migrations, seeders e testes
+
+🚧 Necessário ter o [Docker](https://www.docker.com/) instalado. Os scripts se encontram na raiz do projeto. Será necssario liberar o acesso a porta 3306
+    
+- Script no Windowns: 
+```cmd
+./start.bat
+```
+- Script Linux ou Mac:
+```bash
+chmod +x start.sh
+
+sh start.sh
+
+```
+
+#### Instalação sem script:
+```cmd
+docker-compose up -d --build
+docker-compose exec webapp php artisan migrate --seed
+```
+Acesse: http://localhost:8000/login
+
+#### Instalação sem script e sem docker:
 
 - Instalar as dependências do Composer:
 ```bash
 composer install
 ```
 - Copiar o arquivo .env.example para .env 
-- Verifiquei se as configurações do arquivo .env estão corretas
+- Verifiquei se as configurações do arquivo .env
 
 ```bash
 cp .env.example .env
@@ -35,3 +70,4 @@ npm run dev
 ```bash
 php aritsan serve
 ```
+
---
+++ start.bat
@@ -0,0 +1,32 @@
+@echo off
+echo ----------------------------------------------------
+echo Vou te ajudar a construir o Exata-Tasks
+echo ----------------------------------------------------
+echo Relaxe, cuidamos de tudo!
+echo ----------------------------------------------------
+docker-compose down -v
+echo Construindo a imagem do Conteiner 
+echo ----------------------------------------------------
+docker-compose up -d
+echo ----------------------------------------------------
+echo Conteiner construindo, aguarde mais alguns insta
+echo ----------------------------------------------------
+timeout /t 2 >nul
+docker-compose exec webapp php artisan migrate --seed
+echo ----------------------------------------------------
+cls
+echo ----------------------------------------------------
+echo Rodandos os Tests, com PHPUnit
+echo ----------------------------------------------------
+docker-compose exec webapp php artisan test
+echo ----------------------------------------------------
+docker-compose exec webapp php artisan migrate:ref --seed
+timeout /t 2 >nul
+start chrome http://localhost:8000/login
+echo ----------------------------------------------------
+echo Tudo pronto! 
+echo O Exata Tasks esta disponivel em: http://localhost:8000/login
+echo ----------------------------------------------------
+echo Usuario: admin@admin.com            
+echo Senha: admin123
+echo ----------------------------------------------------
